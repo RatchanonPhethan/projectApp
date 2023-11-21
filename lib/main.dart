45720b44e190485c9c94a2fc601b9acf07fb5369
RatchanonPhethan <ratchanonzk@gmail.com>
2023-11-21 14:13:46 +0700

    แก้ไข

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_project_application/styles/styles.dart';
-import 'package:flutter_project_application/widgets/MenuFooter.dart';
-import 'package:flutter_project_application/widgets/MenuWidget.dart';
-
-import 'Screens/Home.dart';
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-dddd
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,3 +15,5 @@ dddd
     );
   }
 }
+
+class StatelessWidget {}
